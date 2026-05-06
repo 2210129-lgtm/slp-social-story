d265b3b96fe3ff663174d045d3bf6e9a6316ed9f
2210129-lgtm <2210129@slp.education>
2026-05-06 23:33:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import google.generativeai as genai
 st.set_page_config(page_title="사회성 이야기 생성기", page_icon="🌟")
 
 # --- 여기에 본인의 API 키를 직접 입력하세요 ---
-api_key = "AIzaSyCUFh_OMloRrsP21npyPADROvsUChYTD2I" 
+api_key = "AIzaSyDiZqvVqJFoga5oVWKwjVaHKt_yFqjERM0" 
 # ------------------------------------------
 
 # 메인 화면
